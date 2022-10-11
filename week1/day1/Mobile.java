cea5e7a91d603f51505b3c35c299da49da559e26
Deva1411 <115519300+Deva1411@users.noreply.github.com>
2022-10-11 09:55:32 +0530

    Add files via upload
    week1

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName="APPLE";
+		          System.out.println(mobileBrandName);
+			char mobileLogo='i';
+			      System.out.println(mobileLogo);
+			short noOfMobilePiece=2222;
+			      System.out.println(noOfMobilePiece);
+			int modelNumber=297420;
+			      System.out.println(modelNumber);
+			long mobileImeiNumber=831041243479870989L;
+				  System.out.println(mobileImeiNumber);
+			float mobilePrice=49999.99999F;
+			      System.out.println(mobilePrice);
+			boolean isDamaged=false;
+			      System.out.println(isDamaged);
+	}
+
+}
---
+++ week1/day1/TwoWheelers.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class TwoWheelers {
+	public static void main(String[] args) {
+		int noOfWheels=2;
+		
+		short noOfMirrors=2;
+		        System.out.println(noOfMirrors);
+		long chassisNumber=27392619361L;
+				System.out.println(chassisNumber);
+		boolean isPunctured=false;
+		        System.out.println(isPunctured);
+		String bikeName="Yamaha";
+		        System.out.println(bikeName);
+		double runningKM=145.987;
+		        System.out.println(runningKM);
+	}
+
+}
