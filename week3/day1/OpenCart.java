91e66716797f6f2920a0cf56f882122a1a98efc7
Deva1411 <115519300+Deva1411@users.noreply.github.com>
2022-10-26 10:27:49 +0530

    Add files via upload

---
+++ week3/day1/OpenCart.java
@@ -0,0 +1,30 @@
+package selenium.week3.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenCart {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.opencart.com/index.php?route=account/register");
+		driver.findElement(By.name("username")).sendKeys("deva1614");
+		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Deva");
+		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("S");
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("jagandeva1604@gmail.com");
+
+		WebElement country_id=driver.findElement(By.name("country_id"));
+		Select dd1= new Select(country_id);
+		dd1.deselectByVisibleText("India");
+		
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("deva1411");
+		
+		
+		
+	}
+
+}
